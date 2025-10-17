87dfffb8d2a95ed3408867982ff0a7c234c471c9
BiandMix <thaolinh25032005@gmail.com>
2025-10-17 13:04:08 +0700

    Update train_CBL.py

--- classification/train_CBL.py
+++ classification/train_CBL.py
@@ -100,9 +100,9 @@ if __name__ == "__main__":
     if args.dataset == 'dbpedia_14':
         encoded_train_dataset = encoded_train_dataset.remove_columns(['title'])
     if args.dataset == 'Linhduongcute/SOF':
-        unique_labels = encoded_sim_train_dataset.unique('label')
+        unique_labels = encoded_train_dataset.unique('label')
         label2id = {label: idx for idx, label in enumerate(sorted(unique_labels))}
-        encoded_sim_train_dataset = encoded_sim_train_dataset.map(lambda e: {"label": label2id[e["label"]]})
+        encoded_train_dataset = encoded_train_dataset.map(lambda e: {"label": label2id[e["label"]]})
     encoded_train_dataset = encoded_train_dataset[:len(encoded_train_dataset)]
 
     if args.dataset == 'SetFit/sst2':
@@ -115,9 +115,9 @@ if __name__ == "__main__":
         if args.dataset == 'dbpedia_14':
             encoded_val_dataset = encoded_val_dataset.remove_columns(['title'])
         if args.dataset == 'Linhduongcute/SOF':
-            unique_labels = encoded_sim_train_dataset.unique('label')
+            unique_labels = encoded_val_dataset.unique('label')
             label2id = {label: idx for idx, label in enumerate(sorted(unique_labels))}
-            encoded_sim_train_dataset = encoded_sim_train_dataset.map(lambda e: {"label": label2id[e["label"]]})
+            encoded_val_dataset = encoded_val_dataset.map(lambda e: {"label": label2id[e["label"]]})
         encoded_val_dataset = encoded_val_dataset[:len(encoded_val_dataset)]
 
 
@@ -293,3 +293,4 @@ if __name__ == "__main__":
     end = time.time()
 
     print("time of training CBL:", (end - start) / 3600, "hours")
+
